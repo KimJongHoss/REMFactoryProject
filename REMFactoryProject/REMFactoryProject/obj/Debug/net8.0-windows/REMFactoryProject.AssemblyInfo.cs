993e3176d71f70634a0f360adab9563955e9a1b0
RYUBoY <ryusla@naver.com>
2024-08-05 12:05:44 +0900

    pull 받기 용 commit

--- REMFactoryProject/REMFactoryProject/obj/Debug/net8.0-windows/REMFactoryProject.AssemblyInfo.cs
+++ REMFactoryProject/REMFactoryProject/obj/Debug/net8.0-windows/REMFactoryProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("REMFactoryProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d43d35b9d21d147866015669260fe000844e54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d8977b9e95942659e111ab076deb241a13ec6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("REMFactoryProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("REMFactoryProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
