d8d8977b9e95942659e111ab076deb241a13ec6a
RYUBoY <ryusla@naver.com>
2024-08-05 11:26:39 +0900

    tab1 그래프 입력

--- REMFactoryProject/REMFactoryProject/MainWindow.xaml.cs
+++ REMFactoryProject/REMFactoryProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Data.Analysis;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +10,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace REMFactoryProject
 {
     /// <summary>
@@ -16,13 +18,11 @@ namespace REMFactoryProject
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 9, 16, 25 };
-            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
-            WpfPlot1.Refresh();
+            chart1();
         }
 
         private void slider_valueChanged(object sender, RoutedEventArgs e)
--- REMFactoryProject/REMFactoryProject/obj/Debug/net8.0-windows/App.g.i.cs
+++ REMFactoryProject/REMFactoryProject/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace REMFactoryProject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace REMFactoryProject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.6.0")]
         public static void Main() {
             REMFactoryProject.App app = new REMFactoryProject.App();
             app.InitializeComponent();
